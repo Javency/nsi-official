a5c029fb3ca1510351690fcaddaeed34d15da13a
javency <635142482@qq.com>
2018-06-22 00:48:52 +0800

    增加新闻列表分类 优化新闻列表

--- src/pages/InformationPage.vue
+++ src/pages/InformationPage.vue
@@ -2,28 +2,111 @@
     <div class="infoCenter-com">
         <!-- 头条新闻 -->
         <news-banner class="pt60"></news-banner>
+        <!-- 新闻nav -->
+        <ul class="news-nav mt50">
+          <li><router-link to=/InformationCenter/newestArticle>最新文章</router-link><i class="line"></i></li>
+          <li><router-link to=/InformationCenter/original>独家原创</router-link><i class="line"></i></li>
+          <li><router-link to="/InformationCenter/visitSchool">访校文章</router-link><i class="line"></i></li>
+          <li><router-link to="/InformationCenter/policy">政策解读</router-link><i class="line"></i></li>
+        </ul>
         <!-- 新闻列表 -->
-        <news-list class="mt50"></news-list>
+        <transition name="slide-fade">
+            <router-view></router-view>
+        </transition>
     </div>
 </template>
 
 <script>
 import NewsBanner from '../components/information/newsbanner'
-import NewsList from '../components/information/newList'
 export default {
     components:{
-        NewsBanner,
-        NewsList
+        NewsBanner
     }
 }
 </script>
 
 <style lang="scss">
-    .pt60{
+    body{
+      overflow-x: hidden;
+    }
+    .infoCenter-com{
+      .slide-fade{
+          position: absolute;left:0;right: 0;
+        }
+        .slide-fade-enter-active {
+          transition: all 1.2s ease;
+        }
+        .slide-fade-leave-active {
+          transition: all .1s cubic-bezier(2.0, 0.5, 0.8, 1.0);
+        }
+        .slide-fade-enter, .slide-fade-leave-to
+        {
+          left:0;right: 0;
+          transform: translateX(50px);
+          opacity: 0;
+        }
+        .pt60{
+            padding-top: 60px;
+        }
+        .mt50{
+            margin-top: 50px;
+        }
+      .news-nav{
+        padding-left: 0;
+        list-style: none;
         padding-top: 60px;
+        padding-bottom: 20px;
+        background: #fafafa;
+        margin-bottom: 0;
+        text-align: center;
+        li{
+          display: inline-block;
+          position: relative;
+          margin: 0 40px;
+          padding: 5px;
+          .line{
+            display: block;
+            position: absolute;
+            bottom: -10px;
+            left: 0;
+            content: "";
+            background: #333;
+            width: 100%;
+            height: 2px;
+            -webkit-transition: all 1s ease 0s;
+            -ms-transition: all 1s ease 0s;
+            -o-transition: all 0.3s ease 0s;
+            transition: all 0.3s ease 0s;
+            transform: rotateY(90deg);
+          }
+          &:hover .line
+            {
+              transform: rotateY(0);
+            }
+          .router-link-active {
+              text-decoration: none;
+              font-weight: 600;
+              color: #333;
+              ~.line{
+                transform: rotateY(0);
+              }
+          }
+          a{
+            display: inline-block;
+            font-weight: 500;
+            color: #999;
+            font-size: 20px;
+            -webkit-transition: all 1s ease 0s;
+            -ms-transition: all 1s ease 0s;
+            -o-transition: all 0.3s ease 0s;
+            transition: all 0.3s ease 0s;
+            &:hover{
+              text-decoration: none;
+              font-weight: 600;
+              color: #333;
+            }
+          }
+        }
+      }
     }
-    .mt50{
-        margin-top: 50px;
-    }
-    .infoCenter-com{}
 </style>
--- src/routers/router.js
+++ src/routers/router.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import IndexPage from '../pages/IndexPage.vue'
 import InformationPage from '../pages/InformationPage.vue'
+import newestList from '../components/information/newList.vue'
+import original from '../components/information/original.vue'
+import visitschool from '../components/information/visitSchool.vue'
+import policy from '../components/information/policy.vue'
 
 Vue.use(Router)
 
@@ -11,7 +15,25 @@ const routes = [{
     },
     {
         path: "/InformationCenter",
-        component: InformationPage
+        redirect: "/InformationCenter/newestArticle",
+        component: InformationPage,
+        children: [{
+                path: "newestArticle",
+                component: newestList
+            },
+            {
+                path: "original",
+                component: original
+            },
+            {
+                path: "visitSchool",
+                component: visitschool
+            },
+            {
+                path: "policy",
+                component: policy
+            }
+        ]
     }
 ]
 
