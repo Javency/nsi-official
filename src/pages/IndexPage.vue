b2b8595d0121a237b62997405274e9482636ea55
javency <635142482@qq.com>
2018-08-07 23:52:22 +0800

    首页微信分享

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -111,7 +111,7 @@
     import newsInfoM from '../components/index/newsInfo-M.vue'
     import recentEvent from '../components/index/recentEvent-M.vue'
     import bannerM from '../components/index/banner-M.vue'
-    import wx from 'weixin-js-sdk'
+    import wxShareInit from '../assets/js/weChatShare.js'
     var currentSerialNum=0;
     export default {
         components: {
@@ -147,69 +147,17 @@
             },
             // 微信分享
             wxInit(){
-                this.axios({
-                    method:"get",
-                    url:'/Admin_api?whereFrom=WeChatShare&Callback=',
-                    params:{
-                        URL: window.location.href
-                    }
-                }).then((res)=>{
-                    var data=JSON.parse(res.data.split('(')[1].split(')')[0])
-                    // console.log(JSON.parse(res.data.split('(')[1].split(')')[0]))
-                    wx.config({
-                        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                        appId: data.appId, // 必填，公众号的唯一标识
-                        timestamp: data.timestamp, // 必填，生成签名的时间戳
-                        nonceStr: data.nonceStr, // 必填，生成签名的随机串
-                        signature: data.signature,// 必填，签名，见附录1
-                        jsApiList: ['onMenuShareAppMessage', 'onMenuShareTimeline'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-                    })
-                    wx.ready(()=> {
-
-                    // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-                    // 分享给朋友
-                        wx.onMenuShareAppMessage({
-
-                            title: this.wxShareInfo.title, // 分享标题
-
-                            desc: this.wxShareInfo.desc, // 分享描述
-
-                            link: this.wxShareInfo.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-
-                            imgUrl: this.wxShareInfo.imgUrl, // 分享图标
-
-                            type: '', // 分享类型,music、video或link，不填默认为link
-
-                            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-
-                            success: function() {
-                                // 用户确认分享后执行的回调函数
-
-                            },
-                            cancel: function() {
-                                // 用户取消分享后执行的回调函数
-                            }
-                        });
-
-                        // 分享到朋友圈
-                        wx.onMenuShareTimeline({
-
-                            title: this.wxShareInfo.title, // 分享标题
-
-                            link: this.wxShareInfo.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-
-                            imgUrl: this.wxShareInfo.imgUrl, // 分享图标
-
-                            success: function() {
-                                // 用户确认分享后执行的回调函数
-                            },
-                            cancel: function() {
-                                // 用户取消分享后执行的回调函数
-                            }
-                        });
-                    });
-                })
-            }
+              this.axios({
+                  method:"get",
+                  url:'/Admin_api?whereFrom=WeChatShare&Callback=',
+                  params:{
+                      URL: window.location.href
+                  }
+              }).then((res)=>{
+                  wxShareInit.wxConfig(res)
+                  wxShareInit.wxReady(this.wxShareInfo)
+              })
+          }
         },
         mounted(){
             // 近期活动
