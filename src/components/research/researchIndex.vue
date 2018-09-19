8c19b8bd2cdaa98d87c1f6d5c7533189d0f638d1
javency <635142482@qq.com>
2018-09-19 17:16:13 +0800

    优化研究院首页

--- src/components/research/researchIndex.vue
+++ src/components/research/researchIndex.vue
@@ -8,32 +8,36 @@
                 <div class="col-md-4">
                     <div class="Item item01">
                         <h3 class="text-center item-title">新学说<span>国际学校校长研修班</span></h3>
-                        <p class="text-cneter item-apply"><a href="javasctipt:;">立即报名</a></p>
+                        <p class="text-cneter item-apply"><a href="https://jinshuju.net/f/a4LhEl" target="_blank">立即报名</a></p>
                     </div>
                     <div class="row">
                         <div class="col-md-6 col-xs-6 subItem pr7">
-                            <img src="../../assets/img/research/subItem.jpg" alt="">
-                            <p class="subItem-desc">这是课程班级介绍这是课程班级介绍这是课程班级</p>
+                            <img src="../../assets/img/research/subItem003.png" alt="">
+                            <p class="subItem-title">国际学校办学政策</p>
+                            <p class="subItem-desc">国际学校关键环节和部门的营运特质和工作要点，宏观与实操相结合</p>
                         </div>
                         <div class="col-md-6 col-xs-6 subItem pl7">
-                            <img src="../../assets/img/research/subItem.jpg" alt="">
-                            <p class="subItem-desc">这是课程班级介绍这是课程班级介绍这是课程班级</p>
+                            <img src="../../assets/img/research/subItem004.png" alt="">
+                            <p class="subItem-title">国际学校品牌和集团化发展</p>
+                            <p class="subItem-desc">对国际学校的理解更加通透，管理更加自如</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="Item item02">
                         <h3 class="text-center item-title">新学说<span>国际学校投资战略班</span></h3>
-                        <p class="text-cneter item-apply"><a href="javasctipt:;">立即报名</a></p>
+                        <p class="text-cneter item-apply"><a href="https://jinshuju.net/f/UbcHrR" target="_blank">立即报名</a></p>
                     </div>
                     <div class="row">
                         <div class="col-md-6 col-xs-6 subItem pr7">
-                            <img src="../../assets/img/research/subItem01.jpg" alt="">
-                            <p class="subItem-desc">这是课程班级介绍这是课程班级介绍这是课程班级</p>
+                            <img src="../../assets/img/research/subItem001.jpg" alt="">
+                            <p class="subItem-title">探寻本质</p>
+                            <p class="subItem-desc">国际化学校商业模式研究</p>
                         </div>
                         <div class="col-md-6 col-xs-6 subItem pl7">
-                            <img src="../../assets/img/research/subItem02.jpg" alt="">
-                            <p class="subItem-desc">这是课程班级介绍这是课程班级介绍这是课程班级</p>
+                            <img src="../../assets/img/research/subItem002.jpg" alt="">
+                            <p class="subItem-title">寻求方向</p>
+                            <p class="subItem-desc">国际化学校办学市场进入策略</p>
                         </div>
                     </div>
                 </div>
@@ -45,11 +49,13 @@
                     <div class="row">
                         <div class="col-md-6 col-xs-6 subItem pr7">
                             <img src="../../assets/img/research/subItem04.jpg" alt="">
-                            <p class="subItem-desc">这是课程班级介绍这是课程班级介绍这是课程班级</p>
+                            <p class="subItem-title">国际学校HR管理课程</p>
+                            <p class="subItem-desc">提升学校在市场竞争中的识别度，进而实施有效的信息传播。</p>
                         </div>
                         <div class="col-md-6 col-xs-6 subItem pl7">
                             <img src="../../assets/img/research/subItem05.jpg" alt="">
-                            <p class="subItem-desc">这是课程班级介绍这是课程班级介绍这是课程班级</p>
+                            <p class="subItem-title">国际学校市场招生课程</p>
+                            <p class="subItem-desc">一站式解决国际化学校运营各层面人才职业培训和能力提升</p>
                         </div>
                     </div>
                 </div>
@@ -77,9 +83,9 @@
                     </a>
                 </div>
                 <div class="col-md-3 col-xs-6 reportlist">
-                    <h5 class="text-center class-title">课程简介</h5>
-                    <p class="class-info">这是课程简介这是课程简介这是课程简介这是课程简介这是课程简介这是课程简介这是课程简介这是课程简介这是课程简介这是课程简介这是课程简介这是课程简介</p>
-                    <p class="text-center class-apply"><a href="javascript:;">资源下载</a></p>
+                    <h5 class="text-center class-title">关于研究院</h5>
+                    <p class="class-info">新学说研究院是包括行业专家共同研究编撰国际学校内参，国际学校发展报告，行业研究报告，国际学校职能课程，国际学校研修班等</p>
+                    <p class="text-center class-apply"><router-link tag="a" :to="{path:'/research/periodical'}">资源下载</router-link></p>
                 </div>
             </div>
             <!-- 研究成果 END-->
@@ -90,31 +96,34 @@
                 </div>
             </div>
             <div class="row pt30">
-                <div class="col-md-3 col-xs-6 text-center reportlist">
-                    <a href="javascript:;">
+                <div class="col-md-3 col-xs-6 reportlist">
+                    <a href="http://class.xinxueshuo.cn/nsi-class/pages/mac.html" target="_blank">
                         <img src="../../assets/img/research/subItem01.jpg" alt="">
                     </a>
-                    <p class="class-desc">这是课程班级介绍这是课程班级介绍这是课程班级这是课程班级介绍这是课程班级</p>
-                    <p class="text-left class-time">开课时间：2018-09-18</p>
+                    <p class="subItem-title">市场分析</p>
+                    <p class="subItem-desc">中国国际学校市场现状分析以及如何做好国际学校市场的竞品分析</p>
+                    <p class="text-left class-time">开课时间：2018-09-04</p>
                 </div>
-                <div class="col-md-3 col-xs-6 text-center reportlist">
-                    <a href="javascript:;">
+                <div class="col-md-3 col-xs-6 reportlist">
+                    <a href="http://class.xinxueshuo.cn/nsi-class/pages/mac.html" target="_blank">
                         <img src="../../assets/img/research/subItem02.jpg" alt="">
                     </a>
-                    <p class="class-desc">这是课程班级介绍这是课程班级介绍这是课程班级这是课程班级介绍这是课程班级</p>
-                    <p class="text-left class-time">开课时间：2018-09-18</p>
-                </div>
-                <div class="col-md-3 col-xs-6 text-center reportlist">
-                    <a href="javascript:;">
-                        <img src="../../assets/img/research/subItem03.jpg" alt="">
-                    </a>
-                    <p class="class-desc">这是课程班级介绍这是课程班级介绍这是课程班级这是课程班级介绍这是课程班级</p>
+                    <p class="subItem-title">品牌建设</p>
+                    <p class="subItem-desc">国际学校的0到1--品牌定位；三生万物--品牌建设方法论 </p>
                     <p class="text-left class-time">开课时间：2018-09-18</p>
                 </div>
                 <div class="col-md-3 col-xs-6 reportlist">
-                    <h5 class="text-center class-title">课程简介</h5>
-                    <p class="class-info">这是课程简介这是课程简介这是课程简介这是课程简介程简程简介这是课这是课程简介</p>
-                    <p class="text-center class-apply"><a href="javascript:;">立即报名</a></p>
+                    <a href="http://class.xinxueshuo.cn/nsi-class/pages/mac.html" target="_blank">
+                        <img src="../../assets/img/research/subItem03.jpg" alt="">
+                    </a>
+                    <p class="subItem-title">招生策略</p>
+                    <p class="subItem-desc">利用合作方定位和强化自身品牌影响 ；招生人员的基本职业素养 </p>
+                    <p class="text-left class-time">开课时间：2018-10-23</p>
+                </div>
+                <div class="col-md-3 col-xs-6 reportlist">
+                    <h5 class="text-center class-title">关于职能课程</h5>
+                    <p class="class-info">在国际教育行业内，不仅需要优秀的教学管理，对于市场招生环节的重视，更是关系到学校品牌和生源规模的根本。</p>
+                    <p class="text-center class-apply"><a href="http://class.xinxueshuo.cn/nsi-class/pages/mac.html" target="_blank">立即报名</a></p>
                 </div>
             </div>
             <!-- 培训课程 END-->
@@ -125,31 +134,33 @@
                 </div>
             </div>
             <div class="row pt30">
-                <div class="col-md-3 col-xs-6 text-center reportlist">
-                    <a href="javascript:;">
+                <div class="col-md-3 col-xs-6 reportlist">
+                    <a href="http://data.xinxueshuo.cn/nsi/school/school.html" target="_blank">
                         <img src="../../assets/img/research/database01.jpg" alt="">
                     </a>
-                    <p class="class-desc">这是课程班级介绍这是课程班级介绍这是课程班级这是课程班级介绍这是课程班级</p>
-                    <p class="text-left class-time">开课时间：2018-09-18</p>
-                </div>
-                <div class="col-md-3 col-xs-6 text-center reportlist">
-                    <a href="javascript:;">
-                        <img src="../../assets/img/research/database02.jpg" alt="">
-                    </a>
-                    <p class="class-desc">这是课程班级介绍这是课程班级介绍这是课程班级这是课程班级介绍这是课程班级</p>
-                    <p class="text-left class-time">开课时间：2018-09-18</p>
-                </div>
-                <div class="col-md-3 col-xs-6 text-center reportlist">
-                    <a href="javascript:;">
-                        <img src="../../assets/img/research/database03.jpg" alt="">
-                    </a>
-                    <p class="class-desc">这是课程班级介绍这课程班介绍这是课程班级这是课程班级介绍这是课程班级</p>
-                    <p class="text-left class-time">开课时间：2018-09-18</p>
+                    <p class="subItem-title">国际学校库</p>
+                    <p class="subItem-desc">国际学校数据库，为国际学校提供科学的行业数据分析。掌握行业的发展态势，了解自身的成长阶段</p>
                 </div>
                 <div class="col-md-3 col-xs-6 reportlist">
-                    <h5 class="text-center class-title">课程简介</h5>
-                    <p class="class-info">这是课程简介这是课程简介这是课程简介这是课程简程简介这是课程简介</p>
-                    <p class="text-center class-apply"><a href="javascript:;">立即报名</a></p>
+                    <a href="http://data.xinxueshuo.cn/nsi/company/company.html" target="_blank">
+                        <img src="../../assets/img/research/database02.jpg" alt="">
+                    </a>
+                    <p class="subItem-title">国际学校服务机构库</p>
+                    <p class="subItem-desc">国际教育机构数据库提供与国际学校国际教育相关的各类服务机构和服务提供商的详细信息</p>
+                </div>
+                <div class="col-md-3 col-xs-6 reportlist">
+                    <a href="http://data.xinxueshuo.cn/nsi/talent/talent.html" target="_blank">
+                        <img src="../../assets/img/research/database03.jpg" alt="">
+                    </a>
+                    <p class="subItem-title">国际学校人才库</p>
+                    <p class="subItem-desc">国际教育人才数据为内部数据库，上传个人简历，进入国际教育人才储备库，获得更多的展示自己的机会</p>
+                </div>
+                <div class="col-md-3 col-xs-6 reportlist">
+                    <a href="http://data.xinxueshuo.cn/nsi/project/project.html" target="_blank">
+                        <img src="../../assets/img/research/database03.jpg" alt="">
+                    </a>
+                    <p class="subItem-title">国际学校项目库</p>
+                    <p class="subItem-desc">新学说国际学校四库全书，为学校，机构，和从业者提供最权威的行业数据，最先进的行业服务</p>
                 </div>
             </div>
             <!-- 新学说四库全书 END-->
@@ -232,12 +243,20 @@ export default {
            img{
                 border-radius: 4px;
            }
-           .subItem-desc{
-               line-height: 25px;
-               color: #333;
-               font-size: 14px;
-           }
        }
+       .subItem-title{
+               font-weight: 700;
+               margin-top: 10px;
+               color: #202020;
+               margin-bottom: 0;
+               font-size: 15px;
+        }
+        .subItem-desc{
+            line-height: 25px;
+            color: rgb(126, 126, 126);
+            font-size: 14px;
+            margin-bottom: 5px;
+        }
 
         // 研究成果
         .research-title{
@@ -282,7 +301,7 @@ export default {
                 text-align: left;
             }
             .class-time{
-                color: rgb(143, 143, 143);
+                color: rgb(82, 82, 82);
             }
         }
         .class-title{
